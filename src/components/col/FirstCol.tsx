4ec431e88f98c35e521381da370198692a5fb50e
semoltaz <semoltaz@gmail.com>
2025-11-08 23:54:18 -0600

    Fixed Text Color

--- src/components/col/FirstCol.tsx
+++ src/components/col/FirstCol.tsx
@@ -5,7 +5,7 @@ function FirstCol() {
   return (
     <div className='flex flex-col flex-1/3 gap-4 h-full rounded-3xl'>
         <div className='bg-purple-300 rounded-3xl p-4 flex-[2] flex justify-around items-center'>image here</div>
-        <Link to="/about" className='bg-[#D4C7B4] rounded-3xl p-4 flex-[1] flex justify-around items-center text-[#4B4A45] text-2xl font-kalnia'>About Me</Link>
+        <Link to="/about" className='bg-[#D4C7B4] rounded-3xl p-4 flex-[1] flex justify-around items-center text-[#1d1d1d] text-2xl font-kalnia'>About Me</Link>
     </div>
   )
 }
