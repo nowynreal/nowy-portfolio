214d87efdd60152ad5430f5266b027be4773a359
semoltaz <semoltaz@gmail.com>
2025-11-19 14:05:17 -0600

    Updated Home Button

--- src/components/NavBarAlt.tsx
+++ src/components/NavBarAlt.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             Semih YUCEKAN
         </div>
         <div className='px-24 flex gap-14 text-md uppercase poppins-bold h-full items-center'>
-            <Link to="/" className='text-black px-6 py-4 bg-linear-to-tr from-[#1d1d1d] via-[#fff] to-[#1d1d1d] rounded-2xl'>Home</Link>
+            <Link to="/" className='text-white px-6 py-4 bg-linear-to-tr from-[#1d1d1d] via-black to-[#1d1d1d] border-1 border-[#ffffff20] shadow-[0_0_50px_rgba(0,0,0,0)] hover:shadow-[0_0_3px_rgba(255,255,255,1)] transition duration-250 rounded-2xl'>Home</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
--- src/components/col/FirstCol.tsx
+++ src/components/col/FirstCol.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function FirstCol() {
   return (
     <div className='flex flex-col flex-1/3 gap-4 h-full rounded-3xl'>
-        <div className='bg-purple-300 rounded-3xl p-4 flex-[2] flex justify-around items-center'>image here</div>
+        <div className='bg-stone-300 rounded-3xl p-4 flex-[2] flex justify-around items-center'><div className='-skew-5'>image here</div></div>
         <Link to="/about" className='bg-[#D4C7B4] rounded-3xl p-4 flex-[1] flex justify-around items-center text-[#1d1d1d] text-2xl font-kalnia'>About Me</Link>
     </div>
   )
