f75fd2e56f749134e5aebeeee2434b2d842b04a4
semoltaz <semoltaz@gmail.com>
2026-01-10 02:36:00 -0600

    Started Projects Page

---
+++ src/components/ProjectsItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectsItem() {
+  return (
+    <div className="bg-amber-700 w-full h-auto p-4">
+      <h2 className="text-2xl p-4 font-kalnia">Project Title</h2>
+    </div>
+  );
+}
+
+export default ProjectsItem;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,16 @@
+import NavbarAlt from "../components/NavbarAlt";
+import ProjectsItem from "../components/ProjectsItem";
 
 function Projects() {
   return (
-    <div className='h-screen bg-[#1d1d1d] text-amber-300'>dev:Projects</div>
-  )
+    <div className="h-screen bg-[#1d1d1d] text-amber-50">
+      <NavbarAlt />
+      <div className="flex flex-col items-center h-full gap-4">
+        <ProjectsItem />
+        <ProjectsItem />
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
